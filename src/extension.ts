6ad61da5a1187ac66ed5df0a2d0b3f659575bc9a
chyun <746804915@qq.com>
2022-09-19 06:18:06 +0000

    refactor:代码调整

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,55 @@
-// import { http } from 'http';
-const http = require("http");
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { getSign, joinParams } from "./utils";
-import api from "./config";
+import * as vscode from 'vscode'
+import { getSign, joinParams } from './utils'
+import api from './config'
+const http = require('http')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerTextEditorCommand(
-    "var-named-helper.spellhelper",
+    'var-named-helper.spellhelper',
     (textEditor, edit) => {
-      const document: vscode.TextDocument = textEditor.document;
-      const { start, end } = textEditor.selection;
-      const range = new vscode.Range(start, end);
-      const selectContent = document.getText(range);
-      if (!selectContent) {
-        vscode.window.showInformationMessage(`请划取要翻译的词`);
-        return;
+      const selectContent = _getSelectContent(textEditor)
+      if (selectContent) {
+        vscode.window.showInformationMessage(`请划取要翻译的词`)
+        return
       }
 
-      const randomNum = +new Date();
-      const key = joinParams(selectContent, randomNum);
-      const sign = getSign(key);
-      const url = api[0].api(selectContent, randomNum, sign);
+      const randomNum = +new Date()
+      const key = joinParams(selectContent, randomNum)
+      const sign = getSign(key)
+      const url = api[0].api(selectContent, randomNum, sign)
 
       http.get(url, (res: any) => {
         if (res.statusCode !== 200) {
-          console.log(`statusCode ${res.statusCode}`);
-          return;
+          console.log(`statusCode ${res.statusCode}`)
+          return
         }
 
-        let data = "";
-        res.on("data", (chunk: any) => (data += chunk));
-        res.on("end", () => {
-          const parsedData = JSON.parse(data);
-          console.log("data", parsedData);
+        let data = ''
+        res.on('data', (chunk: any) => (data += chunk))
+        res.on('end', () => {
+          const parsedData = JSON.parse(data)
+          console.log('data', parsedData)
 
-          const translateRes = parsedData.trans_result[0].dst;
-          vscode.window.showInformationMessage(`翻译结果：${translateRes}`);
-        });
-      });
+          const translateRes = parsedData.trans_result[0].dst
+          vscode.window.showInformationMessage(`翻译结果：${translateRes}`)
+        })
+      })
     }
-  );
-  context.subscriptions.push(disposable);
+  )
+  context.subscriptions.push(disposable)
+}
+
+function _getSelectContent(textEditor: vscode.TextEditor): string {
+  const document: vscode.TextDocument = textEditor.document
+  const { start, end } = textEditor.selection
+  const range = new vscode.Range(start, end)
+  const selectContent = document.getText(range)
+  if (!selectContent) {
+    return ''
+  }
+  return selectContent
 }
 
 // this method is called when your extension is deactivated
